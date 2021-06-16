8d7b7805db6a9f0a8e82e8d12941d4d1f91ac481
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 19:04:05 +0300

    Tooltip in JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -26,26 +26,15 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) {
-        TreeItem<String> rootTreenode = new TreeItem<String>("Languages");
+        Label lbl = new Label("Label");
+        lbl.setTooltip(new Tooltip("This is a label"));
 
-        TreeItem<String> germanics = new TreeItem<String>("Germanic");
-        germanics.getChildren().add(new TreeItem<String>("German"));
-        germanics.getChildren().add(new TreeItem<String>("English"));
-
-        TreeItem<String> romans = new TreeItem<String>("Roman");
-        romans.getChildren().add(new TreeItem<String>("French"));
-        germanics.getChildren().add(new TreeItem<String>("Spanish"));
-        romans.getChildren().add(new TreeItem<String>("Italians"));
-
-        rootTreenode.getChildren().add(germanics);
-        rootTreenode.getChildren().add(romans);
-        TreeItem<String> langsTreeView = new TreeView<String>(rootTreenode);
-        langsTreeView.setPreftSize(150,200);
-        FlowPane root = new FlowPane(10,10, langsTreeView);
-
+        Button btn = new Button("Click");
+        btn.setTooltip(new Tooltip("Click the button \nto start an action"));
+        FlowPane root = new FlowPane(10,10,btn,lbl);
         Scene scene = new Scene(root,300,250);
         stage.setScene(scene);
-        stage.setTitle("TreeView in JavaFX");
+        stage.setTitle("Tooltip in JavaFX");
         stage.show();
 
     }
