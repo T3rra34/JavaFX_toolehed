0cf99f0de97a9f78859494b561c538801eda464e
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 19:00:47 +0300

    TreeView in JavaFX

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,12 +10,13 @@ import javafx.scene.layout.FlowPane;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
-import javafx.geometry.Insets;
 import javafx.scene.control.ScrollPane;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
 
 public class Main extends Application {
     int clicks = 0;
@@ -25,15 +26,26 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) {
-        ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
-        ChoiceBox<String> langsChoiceBox = new ChoiceBox<String>(langs);
-        langsChoiceBox.setValue("Java");
-        Label lbl = new Label();
-        langsChoiceBox.setOnAction(event -> lbl.setText(langsChoiceBox.getValue()));
-        FlowPane root = new FlowPane(10,10,langsChoiceBox,lbl);
+        TreeItem<String> rootTreenode = new TreeItem<String>("Languages");
+
+        TreeItem<String> germanics = new TreeItem<String>("Germanic");
+        germanics.getChildren().add(new TreeItem<String>("German"));
+        germanics.getChildren().add(new TreeItem<String>("English"));
+
+        TreeItem<String> romans = new TreeItem<String>("Roman");
+        romans.getChildren().add(new TreeItem<String>("French"));
+        germanics.getChildren().add(new TreeItem<String>("Spanish"));
+        romans.getChildren().add(new TreeItem<String>("Italians"));
+
+        rootTreenode.getChildren().add(germanics);
+        rootTreenode.getChildren().add(romans);
+        TreeItem<String> langsTreeView = new TreeView<String>(rootTreenode);
+        langsTreeView.setPreftSize(150,200);
+        FlowPane root = new FlowPane(10,10, langsTreeView);
+
         Scene scene = new Scene(root,300,250);
         stage.setScene(scene);
-        stage.setTitle("ChoiceBox in JavaFX");
+        stage.setTitle("TreeView in JavaFX");
         stage.show();
 
     }
