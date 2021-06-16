19ab751cefa6cc93dec0c121f10879a50354211c
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 12:38:13 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+
+    public void start(Stage stage) {
+    init(stage);
+    stop(stage);
+    }
+
+    private void stop(Stage stage) {
+    }
+
+    private void init(Stage stage) {
+    }
+}
