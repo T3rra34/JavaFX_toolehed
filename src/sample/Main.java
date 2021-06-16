4d821921b880ccd43bd562f6f2ea6c4cc9ea5550
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 16:44:19 +0300

    HBox kasutamine JavaFX'is

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,13 +8,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 
 import java.util.concurrent.Flow;
 
@@ -25,13 +26,13 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) {
-        Label label1 = new Label("Label1");
-        Label label2 = new Label("Label2");
-
-        FlowPane root = new FlowPane(10 , 10 , label1, label2);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 300 , 150);
-        stage.setTitle("Flowpane in JavaFX");
+        HBox hbox = new HBox();
+        Button btn1 = new Button("Add");
+        Button btn2 = new Button("Remove");
+        hbox.getChildren().addAll(btn1, btn2);
+        Scene scene = new Scene(hbox, 300 , 150);
+        stage.setScene(scene);
+        stage.setTitle("hbox in JavaFX");
         stage.show();
     }
 }
