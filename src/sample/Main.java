68b96df6309802f3740357908b6ebee1b10fc728
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 16:35:35 +0300

    Flowpane'i center kasutamine JavaFX'is

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,9 +3,11 @@ package sample;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -14,30 +16,22 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.layout.FlowPane;
 
+import java.util.concurrent.Flow;
+
 public class Main extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
     
     @Override
-    public void start(Stage Stage) throws Exception {
+    public void start(Stage stage) {
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
 
-        Group root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(root);
-        Stage.setScene(scene);
-        Stage.setTitle("Hello JavaFX");
-        Stage.setWidth(250);
-        Stage.setHeight(250);
-
-        Button btn = new Button();
-        btn.setText("Click");
-
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                btn.setText("You've clicked!");
-
-                Stage.show();
-            }});
+        FlowPane root = new FlowPane(10 , 10 , label1, label2);
+        root.setAlignment(Pos.CENTER);
+        scene scene = new Scene(root, 300 , 150);
+        stage.setTitle("Flowpane in JavaFX");
+        stage.show();
     }
 }
