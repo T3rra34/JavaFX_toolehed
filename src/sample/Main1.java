826c84710088d589fe516f8960d5b9f51d63ffc5
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 17:39:05 +0300

    Gridpane in JavaFX

--- src/sample/Main1.java
+++ src/sample/Main1.java
@@ -9,6 +9,8 @@ import javafx.application.Application;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.geometry.Pos;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
 
 public class Main1 extends Application {
     public static void main(String[] args) {
@@ -17,27 +19,20 @@ public class Main1 extends Application {
 
     @Override
     public void start(Stage stage) {
-        Button topbtn = new Button("Top");
-        AnchorPane.setTopAnchor(topbtn, 10.0);
-        AnchorPane.setLeftAnchor(topbtn, 60.0);
-        AnchorPane.setRightAnchor(topbtn, 60.0);
+        Label first = new Label("First");
+        Label second = new Label("Second");
+        Label third = new Label("Third");
 
-        Button botbtn = new Button("Bot");
-        AnchorPane.setTopAnchor(botbtn, 10.0);
-        AnchorPane.setLeftAnchor(botbtn, 60.0);
-        AnchorPane.setRightAnchor(botbtn, 60.0);
+        GridPane root = new GridPane();
+        root.getColumnConstraints().add(new ColumnConstraints(80));
+        root.getColumnConstraints().add(new ColumnConstraints(150));
+        root.getColumnConstraints().add(new ColumnConstraints(70));
 
-        Button leftbtn = new Button("left");
-        AnchorPane.setTopAnchor(leftbtn, 10.0);
-        AnchorPane.setLeftAnchor(leftbtn, 60.0);
-        AnchorPane.setRightAnchor(leftbtn, 60.0);
-
-        Button rightbtn = new Button("Right");
-        AnchorPane.setTopAnchor(rightbtn, 10.0);
-        AnchorPane.setLeftAnchor(rightbtn, 60.0);
-        AnchorPane.setRightAnchor(rightbtn, 60.0);
-
-        AnchorPane root = new AnchorPane(topbtn,rightbtn,botbtn,leftbtn);
+        root.setGridLinesVisible(true);
+        root.setColumnIndex(first, 0);
+        root.setColumnIndex(second, 1);
+        root.setColumnIndex(third, 2);
+        root.getChildren().addAll(first,second,third);
 
         Scene scene = new Scene(root , 300 ,150);
         stage.setScene(scene);
