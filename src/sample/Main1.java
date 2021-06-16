9a12b3e5b8803401480a6746dcacf79578cfd3bb
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 16:59:27 +0300

    BorderPane in JavaFX

--- src/sample/Main1.java
+++ src/sample/Main1.java
@@ -8,6 +8,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Group;
@@ -25,27 +26,13 @@ public class Main1 extends Application {
 
     @Override
     public void start(Stage stage) {
-        Button okbtn = new Button("OK");
-        Button cancelbtn = new Button("Cancel");
-        Label lbl = new Label("Select");
+        Button btn = new Button("Hello");
+        BorderPane root = new BorderPane(btn);
 
-        VBox vbox = new VBox(15);
-
-        VBox.setVgrow(okbtn, Priority.ALWAYS);
-        okbtn.setMaxHeight(Double.MAX_VALUE);
-        okbtn.setMaxWidth(100);
-
-        VBox.setVgrow(cancelbtn, Priority.ALWAYS);
-        cancelbtn.setMaxHeight(Double.MAX_VALUE);
-        cancelbtn.setMaxWidth(100);
-
-        vbox.getChildren().addAll(lbl, okbtn, cancelbtn);
-
-        Scene scene = new Scene(vbox, 300,150);
+        Scene scene = new Scene(root,300,150);
         stage.setScene(scene);
 
-        stage.setTitle("VBox in JavaFX");
-
+        stage.setTitle("BorderPane in JavaFX");
         stage.show();
     }
 }
