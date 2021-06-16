9d3c05cdc87b0942170c76062d98714e936b8b08
Henri Kazbekov <kazbekov.henri@khk.ee>
2021-06-16 15:42:57 +0300

    gruppi tegemine

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Group;
 
 public class Main extends Application {
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
     
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello JavaFX");
-        primaryStage.setWidth(250);
-        primaryStage.setHeight(250);
-        primaryStage.show();
+    public void start(Stage Stage) {
+
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        Stage.setScene(scene);
+        Stage.setTitle("Hello JavaFX");
+        Stage.setWidth(250);
+        Stage.setHeight(250);
+        Stage.show();
     }
 }
